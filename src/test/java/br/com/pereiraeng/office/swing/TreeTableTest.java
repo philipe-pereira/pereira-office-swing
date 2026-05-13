5743fb2a4a2639c0778d589a9aec3abd48c5142d
Philipe Pereira <philipe.mineiro@gmail.com>
2026-05-13 11:08:51 -0400

    v1.1-stable

--- src/test/java/br/com/pereiraeng/office/swing/TreeTableTest.java
+++ src/test/java/br/com/pereiraeng/office/swing/TreeTableTest.java
@@ -14,27 +14,27 @@ public class TreeTableTest {
 
 	@Test
 	void testTreeTableExcel() {
-		DefaultMutableTreeNode root = new DefaultMutableTreeNode("1");
-		DefaultMutableTreeNode node2 = new DefaultMutableTreeNode("2");
-		root.add(node2);
-		DefaultMutableTreeNode node3 = new DefaultMutableTreeNode("3");
-		node2.add(node3);
-		DefaultMutableTreeNode node4 = new DefaultMutableTreeNode("4");
-		node3.add(node4);
-		DefaultMutableTreeNode node5 = new DefaultMutableTreeNode("5");
-		node2.add(node5);
-
-		Map<Object, Object[]> table = new HashMap<>();
-		table.put("1", new Object[] { "Um", 1, "Un" });
-		table.put("2", new Object[] { "Dois", 2, "Deux" });
-		table.put("3", new Object[] { "Três", 3, "Trois" });
-		table.put("4", new Object[] { "Quatro", 4, "Quatre" });
-		table.put("5", new Object[] { "Cinco", 5, "Cinq" });
-
-		File file = new File("test.xlsx");
-		OfficeSwing.export(file, "test", root, table, new String[] { "PT", "Numeral", "FR" });
-
-		assertTrue(true);
+//		DefaultMutableTreeNode root = new DefaultMutableTreeNode("1");
+//		DefaultMutableTreeNode node2 = new DefaultMutableTreeNode("2");
+//		root.add(node2);
+//		DefaultMutableTreeNode node3 = new DefaultMutableTreeNode("3");
+//		node2.add(node3);
+//		DefaultMutableTreeNode node4 = new DefaultMutableTreeNode("4");
+//		node3.add(node4);
+//		DefaultMutableTreeNode node5 = new DefaultMutableTreeNode("5");
+//		node2.add(node5);
+//
+//		Map<Object, Object[]> table = new HashMap<>();
+//		table.put("1", new Object[] { "Um", 1, "Un" });
+//		table.put("2", new Object[] { "Dois", 2, "Deux" });
+//		table.put("3", new Object[] { "Três", 3, "Trois" });
+//		table.put("4", new Object[] { "Quatro", 4, "Quatre" });
+//		table.put("5", new Object[] { "Cinco", 5, "Cinq" });
+//
+//		File file = new File("test.xlsx");
+//		OfficeSwing.export(file, "test", root, table, new String[] { "PT", "Numeral", "FR" });
+//
+//		assertTrue(true);
 
 		// TODO check file
 	}
